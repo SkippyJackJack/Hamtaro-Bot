8af296a0f71fc8cc6f4b4d50860147a9a6bfc115
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-01 13:29:27 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,7 +1,3 @@
-module.exports = {
-	name: 'user-info',
-	description: 'Display info about yourself.',
-	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	},
-};
+exports.run = (client, message, args) => {
+	message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+}
