fbafa4d3f3efa9574ebe67a18e98e6c29b10c6df
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-01 13:28:18 +1000

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,7 +1,3 @@
-module.exports = {
-	name: 'server',
-	description: 'Display info about this server.',
-	execute(message) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	},
-};
+exports.run = (client, message, args) => {
+	message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+}
