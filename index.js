1f10d74f1ed4098276e446be038955521dd5f8e2
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-01 12:43:33 +1000

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-const cooldowns = new Discord.Collection();
-
 client.on('ready', () => {
     // Log to Heroku console
     console.log("---> STATUS: Butler-Bot Online <---");
@@ -26,57 +24,16 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
 
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply('I can\'t execute commands inside DMs!');
-	}
-
-	if (command.args && !args.length) {
-		let reply = `Sorry, but you must provide arguments.`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (!timestamps.has(message.author.id)) {
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-	else {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
+	if (!client.commands.has(command)) return;
 
 	try {
-		command.execute(message, args);
+		client.commands.get(command).execute(message, args);
 	}
 	catch (error) {
 		console.error(error);
-		message.reply('There was an error trying to execute the command.');
+		message.reply('Sorry, but there was an error trying to execute that command.');
 	}
 });
 
