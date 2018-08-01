bea4d2e90c364e2f67030ff31447dcda1965220b
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-01 13:25:38 +1000

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,14 +1,20 @@
-module.exports = {
-	name: 'kick',
-	description: 'Tag a member and kick them (but not really).',
-	guildOnly: true,
-	execute(message) {
-		if (!message.mentions.users.size) {
-			return message.reply('I need a mention of a user to kick.');
-		}
+exports.run = (client, message, [mention, ...reason]) => {
+  const modRole = message.guild.roles.find("name", process.env.MODSROLE);
+  if (!modRole)
+    return message.reply("The " + process.env.MODSROLE + "role does not exist, please edit the MODSROLE environment variable to define a role that has moderator privileges.");
 
-		const taggedUser = message.mentions.users.first();
+  if (!message.member.roles.has(modRole.id))
+    return message.reply("You do not have permission to use this command.");
 
-		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-	},
+  if (message.mentions.members.size === 0)
+    return message.reply("Command requires mention of user to kick \n e.g. !kick @NaughtyMember");
+
+  if (!message.guild.me.hasPermission("KICK_MEMBERS"))
+    return message.reply("You do not have permission to kick members.");
+
+  const kickMember = message.mentions.members.first();
+
+  kickMember.kick(reason.join(" ")).then(member => {
+    message.reply(`Succesfully kicked ${member.user.username}.`);
+  });
 };
