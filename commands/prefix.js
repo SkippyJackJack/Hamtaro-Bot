bece890c146d860d1565bac69cb2c6d73af8744a
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-01 12:50:32 +1000

    Delete prefix.js

--- commands/prefix.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, command, args) => {
-  let newPrefix = message.content.split(" ").slice(1, 2)[0];
-  client.config.prefix = newPrefix;
-  fs.writeFile(client.config, JSON.stringify(client.config), (err) => console.error);
-}
