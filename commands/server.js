176a8b3da680f378ab9e91ae446f266330973a96
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-02 12:38:02 +1000

    Update and rename server.js to serverinfo.js

--- commands/server.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args) => {
-	message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-}
---
+++ commands/serverinfo.js
@@ -0,0 +1,24 @@
+exports.run = (client, message, args) => {
+	message.channel.send({embed: {
+		color: 3447003,
+		author: {
+		  name: client.user.username,
+      		  icon_url: client.user.avatarURL
+		},
+		title: "**Info for ${message.guild.name}**",
+		description: "Here is some information about the current server.",
+		fields: [{
+		    name: "**Members**",
+		    value: "${message.guild.memberCount}"
+		},
+		{
+		    name: "**Created on:**",
+		    value: "${message.guild.createdAt}"
+		},
+		{
+		    name: "**Owner:**",
+		    value: "${message.guild.owner}"
+		}	
+	    ],	
+	}						
+});
