de82c70069d7dd79b2525625babecb9f9bbab67c
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-03 09:26:54 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-	message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	message.channel.send(`**Your username:** ${message.author.username}\n**Your ID:** ${message.author.id}`);
 }
