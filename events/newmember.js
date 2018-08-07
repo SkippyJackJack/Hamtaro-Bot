ccc2641d33351cf92d68c94e8869b1199ae14058
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-07 17:11:40 +1000

    Delete newmember.js

--- events/newmember.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, member) => {
-    client.on('guildMemberAdd', member => {
-    const newMember = member.guild.channels.find('name', 'welcome-channel');
-    if (!channel) return console.log("welcome-channel not found.");
-    channel.send(`Greetings, ${newMember}. Welcome to the server!`);
- })
-};
