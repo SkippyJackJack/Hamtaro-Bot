0c774c7d6bb3ac2b77270272278b2fa1e133348a
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-07 19:24:24 +1000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,4 @@
+const config = require("../config.json");
 exports.run = (client, message, args) => {
-    message.reply("Invite me to your server here: " + client.config.invite-link);
+    message.reply("Invite me to your server here: " + config.invite-link);
 }
