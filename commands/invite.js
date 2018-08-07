420c0fc4a280616243f276871352cd56d4007fda
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-08 09:27:35 +1000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,17 @@
 const config = require("../config.json");
 exports.run = (client, message, args) => {
     message.reply("Invite me to your server here: " + config.invite-link);
-}
+};
+
+exports.conf {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['inv', 'invitelink', 'invlink'],
+    permLevel: 0
+};
+
+exports.help {
+    name: 'invite',
+    description: 'Displays a link that will allow you to invite me to your server!'
+    usage: 'invite'
+};
