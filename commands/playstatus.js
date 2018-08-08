509ca61f1360be850f8233b70e1965d03b982843
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-08 12:33:23 +1000

    Delete playstatus.js

--- commands/playstatus.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client, message, args) => {
-   let newPlayStatus = message.content.split(" ").slice(1, 2)[0];
-   client.user.setActivity('' + newPlayStatus, {type: 'PLAYING'}); 
-  }
