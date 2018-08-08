18dbcf706d18f36229c7bc17b1dc4a49cabea772
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-08 12:59:27 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,16 @@
 exports.run = (client, message, args) => {
 	message.channel.send(`**Your username:** ${message.author.username}\n**Your ID:** ${message.author.id}`);
 }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'userinfo',
+  description: 'Displays information about your user.',
+  usage: 'userinfo'
+};
