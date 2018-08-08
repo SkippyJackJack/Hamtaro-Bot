b1faeddd7902a2ba817079ecd5846b670534b345
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-08 12:33:10 +1000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,16 @@
 exports.run = (client, message, args) => {
         message.channel.send('Pong!').catch(console.error);
 }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ping',
+  description: 'Returns Pong.',
+  usage: 'ping'
+};
