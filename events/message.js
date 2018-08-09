a2c5633f07c1168be7c1ef14c6c4a197bf17c858
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-09 17:10:12 +1000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,21 +5,15 @@ module.exports = (client, message) => {
   // Ignore messages not starting with the prefix (in config.json)
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-  // Standard argument/command name definition.
-  const args = message.content.split(' ').slice(1);
-  const command = message.content.split(' ')[0].slice(client.config.prefix.length);
+  // Our standard argument/command name definition.
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-  // Grab the command data from the client.commands Discord collection
-  let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
+  // Grab the command data from the client.commands Enmap
+  const cmd = client.commands.get(command);
 
-  // If that command doesn't exist, exit and do nothing
-  if (!cmd)
-  	return message.reply("Sorry, but that command doesn't exist");
+  // If that command doesn't exist, silently exit and do nothing
+  if (!cmd) return;
 
   // Run the command
   cmd.run(client, message, args);
