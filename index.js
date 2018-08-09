6bfafc8510cb9fd79b6c3d63bc9db526915f8dc0
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-09 12:48:43 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
 const moment = require("moment");
+const jimp = require("jimp);
 
 const client = new Discord.Client();
 const config = require("./config.json");
