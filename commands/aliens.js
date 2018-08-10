a29d4fe3894407c3283640f96d39531f8715d2b1
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-10 14:14:08 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -7,7 +7,7 @@ const background = await Canvas.loadImage('../images/aliens.jpg');
 ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 const attachment = new Discord.Attachment(canvas.toBuffer(), 'aliens.jpg');
 
-message.channel.send(`Welcome to the server, ${member}!`, attachment);
+message.channel.send(`Here:`, attachment);
 
 };
 
