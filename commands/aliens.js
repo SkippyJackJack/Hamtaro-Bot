d78741405316e048bb274e0d725b8f9be30972ce
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-10 13:29:49 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -1,4 +1,5 @@
-module.exports = async(client, message, args) => {
+const Canvas = require("canvas");
+module.exports = async (client, message, args) => {
 
 const canvas = Canvas.createcanvas(500,436);
 const background = await Canvas.loadImage('../images/aliens.jpg');
@@ -8,4 +9,17 @@ const attachment = new Discord.Attachment(canvas.toBuffer(), 'aliens.jpg');
 
 message.channel.send(`Welcome to the server, ${member}!`, attachment);
 
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['aliensmeme'],
+  permLevel: "0"
+};
+
+exports.help = {
+  name: "aliens",
+  description: "Make your own \"Aliens\" meme.",
+  usage: "aliens [text]"
 };
