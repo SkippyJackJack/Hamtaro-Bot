02774162fa6bafd8911d52e93e601eb1d6a34914
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-11 16:50:06 +1000

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,29 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+
+  if (message.author.id !== client.config.ownerID) return;
+  
+  const code = args.join(" ");
+  
+  try {
+    const evaled = eval(code);
+    const clean = await client.clean(client, evaled);
+    
+    message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+    
+  } catch (err) {
+    message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Bot Owner"
+};
+
+exports.help = {
+  name: "eval",
+  description: "Executes arbitrary javascript. Only usable by the Bot Owner.",
+  usage: "eval [...code]"
+};
