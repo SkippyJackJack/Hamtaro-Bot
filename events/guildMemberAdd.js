009972b48557c37b4f27ba06f6130431760b98a3
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-11 15:41:32 +1000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => { 
-    const channel = member.guild.channels.find('name', client.config.welcome-channel);
+    const channel = message.guild.channels.find('name', client.config.welcome-channel);
     
     if (!channel) return console.log("welcome-channel not found.");
     	channel.send(`Greetings, ${member}. Welcome to the server!`);
