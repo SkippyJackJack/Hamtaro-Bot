af675fd6fdc9c90f4fc190f2cb2428774339b663
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-12 18:32:20 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -1,17 +1,17 @@
-const Canvas = require('canvas');
 exports.run = async (client, message, args) => {
 
    const canvas = Canvas.createCanvas(700, 250);
    const ctx = canvas.getContext('2d');
-   const background = await Canvas.loadImage('../images/aliens.jpg');
-  
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
 
-    channel.send(`Special delivery for $(message.author.user)!`, attachment);
+  // Since the image takes time to load, you should await it
+   const background = await Canvas.loadImage('./wallpaper.jpg');
+  // This uses the canvas dimensions to stretch the image onto the entire canvas
+   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  // Use helpful Attachment class structure to process the file for you
+   const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
 
-};
+   channel.send(`Here:`, attachment);
+});
 
 exports.conf = {
   enabled: true,
