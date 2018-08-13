ca268c3a90f79ea9660f1af39a66773df61e8620
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-13 11:42:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Enmap = require("enmap");
 const fs = require("fs");
 const { promisify } = require("util");
 const moment = require("moment");
-const Canvas = require("canvas");
 
 const client = new Discord.Client();
 const config = require("./config.json");
