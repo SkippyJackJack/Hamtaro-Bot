263169ea421919872e799434b2e782db0002234d
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-14 11:29:13 +1000

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,10 +1,9 @@
 module.exports = (client, message, guild, user) => {
 	
-	if (client.config.announceBans == "false")
-		return;
+	if (client.config.announceBans == "false") return;
 		
 	const channel = message.guild.channels.find('name', client.config.announceBansChannel)
 	
 	if (!channel) return console.log("announceBansChannel not found");
-		channel.send(`${user} was just banned form the server. Ba-Bye!`);
+		channel.send(`${user.username} was just banned form the server!`);
 };
