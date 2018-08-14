056e8ee8c71a5d1b66dd1d441bdc708e03f31ef6
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-14 13:39:24 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -2,15 +2,23 @@ exports.run = (client, message, args) => {
 
 const Jimp = require('jimp');
 
-const url = 'https://www.w3schools.com/w3css/img_lights.jpg';
+var url = message.author.avatarURL;
 
-Jimp.read(url)
-  .then(image => {
-    image.greyscale().getBuffer(Jimp.MIME_JPEG, onBuffer);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+    Jimp.read(url).then(function (image) {
+
+      image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
+
+
+      Jimp.loadFont(doRandomSize()).then(function (font) { // load font from .fnt file
+        // print a message on an image
+        //image.print(font, 2, 2, args.join(" "), Jimp.ALIGN_FONT_CENTER); // print a message on an image with text wrapped at width
+
+
+        image.greyscale()
+
+
+        /**image.greyscale()**/
+        image.print(font, 20, 960, args.join(" "), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
 
   let outputfile = "./output/" + Math.random().toString(36).substr(2, 5) + "sad." + image.getExtension(); // create a random name for the output file
         image.write(outputfile, function () {
