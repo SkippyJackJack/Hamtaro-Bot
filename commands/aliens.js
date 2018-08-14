67b6771050659c0819d2243ae57e84a2845d4e0b
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-14 12:34:27 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -1,92 +1,22 @@
-exports.run = async (client, message, args) => {
+exports.run = (client, message, args) => {
 
-const { createWriteStream } = require('fs');
-const pify = require('pify');
-const imageSizeOf = require('image-size');
-const { createCanvas, loadImage, Image } = require('canvas');
+const Jimp = require('jimp');
 
-const imageSizeOfP = pify(imageSizeOf)
+const url = 'https://www.w3schools.com/w3css/img_lights.jpg';
 
-function createImageFromBuffer (buffer) {
-  const image = new Image()
-  image.src = buffer
+Jimp.read(url)
+  .then(image => {
+    image.greyscale().getBuffer(Jimp.MIME_JPEG, onBuffer);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
-  return image
-}
-
-function createCaptionOverlay ({
-  text,
-  width,
-  height,
-  font = 'Arial',
-  fontSize = 48,
-  captionHeight = 120,
-  decorateCaptionTextFillStyle = null,
-  decorateCaptionFillStyle = null,
-  offsetX = 0,
-  offsetY = 0
-}) {
-  const canvas = createCanvas(width, height)
-  const ctx = canvas.getContext('2d')
-
-  const createGradient = (first, second) => {
-    const grd = ctx.createLinearGradient(width, captionY, width, height)
-    grd.addColorStop(0, first)
-    grd.addColorStop(1, second)
-
-    return grd
+  function onBuffer(err, buffer) {
+    if (err) throw err;
+    const attachment = new Discord.Attachment(buffer, 'aliens.jpg');
+    message.channel.send('Here:', attachment);
   }
-
-  // Hold computed caption position
-  const captionX = offsetX
-  const captionY = offsetY + height - captionHeight
-  const captionTextX = captionX + (width / 2)
-  const captionTextY = captionY + (captionHeight / 2)
-
-  // Fill caption rect
-  ctx.fillStyle = decorateCaptionFillStyle
-    ? decorateCaptionFillStyle(ctx)
-    : createGradient('rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.45)')
-  ctx.fillRect(captionX, captionY, width, captionHeight)
-
-  // Fill caption text
-  ctx.textBaseline = 'middle'
-  ctx.textAlign = 'center'
-  ctx.font = `${fontSize}px ${font}`
-  ctx.fillStyle = decorateCaptionTextFillStyle
-    ? decorateCaptionTextFillStyle(ctx)
-    : 'white'
-  ctx.fillText(text, captionTextX, captionTextY)
-
-  return createImageFromBuffer(canvas.toBuffer())
-}
-
-(async () => {
-  try {
-    const source = '../images/aliens.jpg'
-    const {width, height} = await imageSizeOfP(source)
-    const canvas = createCanvas(width, height)
-    const ctx = canvas.getContext('2d')
-
-    // Draw base image
-    const image = await loadImage(source)
-    ctx.drawImage(image, 0, 0)
-
-    // Draw caption overlay
-    const overlay = await createCaptionOverlay({
-      text: 'Hello!',
-      width,
-      height
-    })
-    ctx.drawImage(overlay, 0, 0)
-
-    // Output to `.png` file
-    canvas.createPNGStream().pipe(createWriteStream('foo.png'))
-  } catch (err) {
-    console.log(err)
-    process.exit(1)
-  }
- })()
 };
 
 exports.conf = {
