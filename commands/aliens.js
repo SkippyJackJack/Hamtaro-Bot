64abd84b113c26974a198a97c88cf8f0b5dec2c4
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-14 14:31:23 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -2,40 +2,53 @@ exports.run = (client, message, args) => {
 
 const Jimp = require('jimp');
 
-var url = message.author.avatarURL;
+    function doRandomSize() {
+    var rand = [Jimp.FONT_SANS_64_BLACK]
+    return rand[Math.floor(Math.random() * rand.length)];
+  }
+    
+    let user = message.author();
+
+    let testingtxt = "hello";
+
+    message.channel.startTyping()
+    var url = user.avatarURL;
 
     Jimp.read(url).then(function (image) {
 
       image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
 
-
       Jimp.loadFont(doRandomSize()).then(function (font) { // load font from .fnt file
         // print a message on an image
         //image.print(font, 2, 2, args.join(" "), Jimp.ALIGN_FONT_CENTER); // print a message on an image with text wrapped at width
 
-
-        image.greyscale()
-
-
         /**image.greyscale()**/
-        image.print(font, 20, 960, args.join(" "), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
+        image.print(font, 20, 960, testingtxt, Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
 
-  let outputfile = "./output/" + Math.random().toString(36).substr(2, 5) + "sad." + image.getExtension(); // create a random name for the output file
+        //image.mask(image, 100, 100).getBuffer(Jimp.MIME_JPEG, onBuffer);
+        let outputfile = "./" + Math.random().toString(36).substr(2, 5) + "." + image.getExtension(); // create a random name for the output file
         image.write(outputfile, function () {
           // upload file
           message.channel.sendFile(outputfile).then(function () {
             // delete file
             fs.unlink(outputfile);
-            console.log("SUCCESS: " + message.author.username);
+            console.log("SUCCESS: " + user.username);
+            message.channel.stopTyping()
           });
         });
+      });
+    }).catch(function (err) {
+      console.error(err);
+      message.reply("Error, Could be that your avatar is invalid OR your avatar has something like <>.")
+    })
 
-  function onBuffer(err, buffer) {
-    if (err) throw err;
-    console.log("buffer");
-  }
-  
-};
+    function onBuffer(err, buffer) {
+      if (err) throw err;
+
+      console.log(buffer);
+
+    }
+  };
 
 exports.conf = {
   enabled: true,
