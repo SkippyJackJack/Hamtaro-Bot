acd66c8b7dea943c184b7be9e7e2408f1f6d2052
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-14 12:37:40 +1000

    Delete ready.js

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-const chalk = require('chalk');
-exports.run = client => {
-  console.log(chalk.bgCyan.black.bold(`Online and active on ${client.guilds.size} servers.`));
-  client.user.setActivity(client.config.prefix + 'help', {type: 'WATCHING'});
-};
