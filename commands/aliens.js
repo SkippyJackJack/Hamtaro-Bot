725c8d54a31c4889aee00a39a9a469381007aae9
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-14 18:17:37 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -2,56 +2,40 @@ exports.run = (client, message, args) => {
 
 const Jimp = require('jimp');
 
-    function doRandomSize() {
-    var rand = [Jimp.FONT_SANS_64_BLACK]
-    return rand[Math.floor(Math.random() * rand.length)];
-  }
-
-    let testingtxt = "hello";
+    let text = args.slice(1).join(" ");
 
     message.channel.startTyping()
-    var url = message.author.avatarURL;
-
-    Jimp.read(url).then(function (image) {
+   
+    Jimp.read(../images/aliens.jpg).then(function (image) {
 
       image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
 
-      Jimp.loadFont(doRandomSize()).then(function (font) { // load font from .fnt file
-        // print a message on an image
-        //image.print(font, 2, 2, args.join(" "), Jimp.ALIGN_FONT_CENTER); // print a message on an image with text wrapped at width
+      Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function(font) { // load font from .fnt file
+        image.print(font, 20, 960, text, Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG)
 
-        /**image.greyscale()**/
-        image.print(font, 20, 960, testingtxt, Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, onBuffer)
-
-        //image.mask(image, 100, 100).getBuffer(Jimp.MIME_JPEG, onBuffer);
         let outputfile = "./" + Math.random().toString(36).substr(2, 5) + "." + image.getExtension(); // create a random name for the output file
         image.write(outputfile, function () {
-          // upload file
-          message.channel.sendFile(outputfile).then(function () {
-            // delete file
+          message.channel.send({
+                            files: [{
+                                attachment: outputfile,
+                                name: output.jpg
+                            }]
+                        }).then(function () {
             fs.unlink(outputfile);
-            console.log("SUCCESS: " + user.username);
             message.channel.stopTyping()
           });
         });
       });
     }).catch(function (err) {
       console.error(err);
-      message.reply("Error, Could be that your avatar is invalid OR your avatar has something like <>.")
+      message.reply(";-; I-I couldn't create the image, sorry.")
     })
-
-    function onBuffer(err, buffer) {
-      if (err) throw err;
-
-      console.log(buffer);
-
-    }
   };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['aliensmeme'],
   permLevel: "0"
 };
 
