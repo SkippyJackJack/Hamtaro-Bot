aaee3daae45427223b0b7756a62394fc4de4a6ab
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-15 13:11:26 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -2,7 +2,13 @@ exports.run = (client, message, args) => {
 
 const fs = require('fs');
 const Jimp = require('jimp');
-
+    
+    function JFont() {
+    var rand = [Jimp.FONT_SANS_64_BLACK]
+    return rand[Math.floor(Math.random() * rand.length)];
+  }
+    message.channel.startTyping();
+    
     let memeText = args.slice(1).join(" ");
     let image = "https://imgflip.com/s/meme/Ancient-Aliens.jpg";
    
@@ -10,8 +16,8 @@ const Jimp = require('jimp');
 
       image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
 
-      Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function(font) {
-        image.print(font, 20, 960, memeText, Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, nullFunction)
+      Jimp.loadFont(JFont()).then(function(font) {
+        image.print(font, 20, 960, args.join(" "), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, nullFunction)
 
         let outputfile = "./" + Math.random().toString(36).substr(2, 5) + "." + image.getExtension();
         image.write(outputfile, function () {
@@ -34,6 +40,8 @@ const Jimp = require('jimp');
          console.log(buffer);
          if (err) throw err;
     }
+    
+    message.channel.stopTyping();
 };
 
 exports.conf = {
