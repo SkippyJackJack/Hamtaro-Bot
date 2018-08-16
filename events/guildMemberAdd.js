8aa845d5b53b337b107a622286634ee02b349a03
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-16 17:11:21 +1000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,11 +3,11 @@ module.exports = (client, message, member) => {
   if (client.config.welcomeNewMembers == "false") return;
   
   const welcomeMessages = [
-    `Everybody our new member ${member} to the server!`,
-    `Welcome to the server ${member}!`,
-    `Please welcome the one and only ${member}!`,
-    `Introduce yourselves to our new member ${member}!`,
-    `Greetings ${member}! Welcome to the server!`
+    `Everybody welcome our new member ${member.user.username} to the server!`,
+    `Welcome to the server ${member.user.username}!`,
+    `Please welcome the one and only ${member.user.username}!`,
+    `Introduce yourselves to our new member ${member.user.username}!`,
+    `Greetings ${member.user.username}! Welcome to the server!`
   ];
   
     var welcomeMessage = Math.floor(Math.random()*welcomeMessages.length);
