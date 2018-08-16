6ac796492e717a6ffda500627767f68801a878a1
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-16 16:14:52 +1000

    Create hoot.js

---
+++ commands/hoot.js
@@ -0,0 +1,36 @@
+exports.run = (client, message, args) => {
+
+const http = require('http');
+
+let options = {
+    host: 'random-images-api.herokuapp.com',
+    path: '/hoot.html'
+}
+var request = http.request(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+        data += chunk;
+    });
+    res.on('end', function () {
+        console.log(data);
+
+    });
+});
+request.on('error', function (e) {
+    console.log(e.message);
+});
+request.end();
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "0"
+};
+
+exports.help = {
+  name: "hoot",
+  description: "Hoo-Hoot! ^o,o^"
+  usage: "hoot"
+};
