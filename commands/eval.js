4ee02e54f71b5f12c70b0e29f1c3f8edbe9c0db9
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-17 11:56:48 +1000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,19 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
 
+  client.clean = async (client, text) => {
+    if (text && text.constructor.name == "Promise")
+      text = await text;
+    if (typeof evaled !== "string")
+      text = require("util").inspect(text, {depth: 1});
+
+    text = text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203))
+      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+
+    return text;
+  };
+  
   if (message.author.id !== client.config.ownerID) return;
   
   const code = args.join(" ");
