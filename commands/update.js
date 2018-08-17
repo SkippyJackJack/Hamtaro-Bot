c75d7a6dc528c8e93ab2f8398ae91f5944160896
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-17 13:27:21 +1000

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
   
- if (message.author.id !== client.config.ownerId) return message.channel.send("Only my creator may push updates ^-^");
+ if (message.author.id !== client.config.ownerId) return message.channel.send("Only my creator may push updates ^-^")
   
  client.fetchUser(client.config.ownerID).then(owner => {
    var ownerAvatar = owner.avatarURL;
- });
+ })
                                               
  const embed = new Discord.RichEmbed()
   .setTitle("Hamtoro Bot is now in Maintenance Mode")
@@ -24,8 +24,7 @@ exports.run = (client, message, args) => {
     "During this time it will still be able to receive commands and respond to them, but please refrain from doing so.")
   .addBlankField(true)
 
-  message.channel.send({embed});
-  }
+  message.channel.send({embed})
 };
 
 exports.conf = {
