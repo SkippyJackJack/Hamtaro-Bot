9006d1bed8ca765a298cf74f23bacbc7095d54b5
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-20 18:01:09 +1000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,12 +2,12 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.send("```${client.commands.map(c => `${client.config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)}: ${c.help.description}`).join('\n')}```");
+    message.author.send(`${client.commands.map(c => `${client.config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)}: ${c.help.description}`).join('\n')}`, "ascii");
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.send("```ascii\n${command.help.name}\n${command.help.description}\nUsage: ${command.help.usage}```");
+      message.author.send(`n${command.help.name}\n${command.help.description}\nUsage: ${command.help.usage}`, "ascii");
     }
   }
 };
