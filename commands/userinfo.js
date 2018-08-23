ba75839f5dfd3e0f236abd67134f852a08903feb
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-23 13:15:26 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args) => {
 	
- let embed = new Discord.RichEmbed()
+ const embed = new Discord.RichEmbed()
   .setAuthor("User Info")
   .setTitle("**Username**")
-  .setDescription(message.author.username)
-  .addField('**ID**', messagel.author.ID, false)
-  .addField('**Account Created**', message.author.createdTimestamp, true)
-  .addField('**Bot Account**', message.author.bot, true)
-  .setThumbnail(message.author.avatarURL)
+  .setDescription(`${message.author.username}`)
+  .addField('**ID**', `${message.author.ID}`, false)
+  .addField('**Account Created**', `${message.author.createdTimestamp}`, true)
+  .addField('**Bot Account**', `${message.author.bot}`, true)
+  .setThumbnail(`${message.author.avatarURL}`)
   .setColor('GREEN')
  
  message.channel.send({embed});
@@ -17,7 +17,6 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['userinf'],
-  permLevel: 0
 };
 
 exports.help = {
