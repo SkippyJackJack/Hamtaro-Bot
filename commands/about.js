8a8513bf94aa37ba64a7da1e8e2b406856dbd0a3
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-24 14:12:10 +1000

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 
   message.channel.send({embed: {
-    color: client.config.embedColour,
+    color: client.config.aboutEmbedColour,
     title: `Hamtoro Bot »|« Release ${client.config.version} [ALPHA]`,
     description: "High-Fox",
     fields: [{
