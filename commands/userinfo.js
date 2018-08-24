4d0ee95dda0096a8703cd9f195954efd9047186c
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-24 16:20:50 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,17 +1,36 @@
-const { RichEmbed } = require("discord.js");
 exports.run = (client, message, args) => {
-	
- const userInfoEmbed = new RichEmbed()
-  .setTitle("**Username**")
-  .setAuthor("User Info")
-  .setColor("GREEN")
-  .setDescription(`${message.author.username}`)
-  .setThumbnail(`${message.author.avatarURL}`)
-  .addField("**ID**", `${message.author.ID}`, false)
-  .addField("**Account Created**", `${message.author.createdTimestamp}`, true)
-  .addField("**Bot Account**", `${message.author.bot}`, true);
   
-   message.channel.send({userInfoEmbed});
+   message.channel.send({
+      embed: {
+	  color: client.config.userInfoEmbedColour,
+	  author: {
+	       name: "User Info",
+	  },
+	  thumbnail: {
+	       url: `${message.author.avatarURL}`,
+	  },
+	  fields: [
+	      {
+		 name: '**Username**',
+		 value: `${message.author.username}`,
+		 inline: true,
+	      },
+	      {
+		 name: '**ID**',
+		 value: `${message.author.id}`,
+		 inline: true,
+	      },
+	      {
+		 name: '**Account Creation Date**',
+		 value: `${message.author.createdTimestamp}`,
+	      },
+	      {
+		 name: '**Bot Account**',
+		 value: `${message.author.bot}`,
+	      },
+	   ],
+        }
+    })
 };
 
 exports.conf = {
