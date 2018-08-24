700f8e90dbbf4aee1d46b4912759379d2bb12017
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-24 13:16:05 +1000

    Update suggestion.js

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -7,6 +7,18 @@ exports.run = async (client, message, args) => {
   let user = client.fetchUser(client.config.ownerID)
    .then(user => {
       // Once promise returns with user, send user a DM
-      user.send(suggestionText); 
+      user.send(`From: ${message.author.username} (${message.author.id})\n` + suggestionText); 
    })
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['suggest']
+};
+
+exports.help = {
+  name: "suggestion",
+  description: "Send a suggestion you have to my master! WARNING: If a user uses this command for anything other than suggesting things, future suggestions from this user will be ignored.",
+  usage: "suggestion [text]"
 };
