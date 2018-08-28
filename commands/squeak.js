848f1d942b3550b2e4cc1be648661ccefaf0d586
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-28 17:14:22 +1000

    Update squeak.js

--- commands/squeak.js
+++ commands/squeak.js
@@ -1,12 +1,12 @@
-exports.run = (client, message, args) => {
+exports.run = (client, message, level) => {
   
   message.channel.send( { embed: { title: "Have a hamster *Squeak!*", color: 0xf29837, image: { url: client.hamtaroFunctions.getRandomImage("hamster"), }, } } )
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: []
+  aliases: [],
+  permLevel: "User"
 };
 
 exports.help = {
