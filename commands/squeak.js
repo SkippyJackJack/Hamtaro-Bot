56773dde25966338d057963a0155c1b824f780da
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-28 12:19:19 +1000

    Update squeak.js

--- commands/squeak.js
+++ commands/squeak.js
@@ -1,9 +1,6 @@
-const hamsterLinks = require("../src/imageArrays/squeak.json");
 exports.run = (client, message, args) => {
   
-  var randomHamsterLink = Math.floor(Math.random() * hamsterLinks.length);
-  
-  message.channel.send( { embed: { title: "Have a hamster *Squeak!*", color: 0xf29837, image: { url: hamsterLinks[randomHamsterLink], }, } } )
+  message.channel.send( { embed: { title: "Have a hamster *Squeak!*", color: 0xf29837, image: { url: client.getRandomImage("hamster"), }, } } )
 };
 
 exports.conf = {
