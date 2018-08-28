d66751102acf52b9f2573760e5005d3df9db4397
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-28 12:18:41 +1000

    Update hoot.js

--- commands/hoot.js
+++ commands/hoot.js
@@ -1,9 +1,8 @@
-const owlLinks = require("../src/imageArrays/hoot.json");
 exports.run = (client, message, args) => {
   
   var randomOwlLink = Math.floor(Math.random() * owlLinks.length);
     
-  message.channel.send( { embed: { title: `Special Delivery for ${message.author.username}!`, color: 0xf29837, image: { url: owlLinks[randomOwlLink], }, } } )
+  message.channel.send( { embed: { title: `Special Delivery for ${message.author.username}!`, color: 0xf29837, image: { url: client.getRandomImage("owl"), }, } } )
 };
 
 exports.conf = {
