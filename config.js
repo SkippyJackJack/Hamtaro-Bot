7ffab1d1386b7595d463a43bc681f1c8b591ec0f
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-28 17:43:23 +1000

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const config = {
       name: "Moderator",
       check: (message) => {
         try {
-          const modRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.modRole.toLowerCase());
+          const modRole = message.guild.roles.find(r => r.name.toLowerCase() === message.serverConfig.modRole.toLowerCase());
           if (modRole && message.member.roles.has(modRole.id)) return true;
         } catch (e) {
           return false;
@@ -63,7 +63,7 @@ const config = {
       name: "Administrator", 
       check: (message) => {
         try {
-          const adminRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.adminRole.toLowerCase());
+          const adminRole = message.guild.roles.find(r => r.name.toLowerCase() === message.serverConfig.adminRole.toLowerCase());
           return (adminRole && message.member.roles.has(adminRole.id));
         } catch (e) {
           return false;
@@ -89,9 +89,13 @@ const config = {
       check: (message) => config.admins.includes(message.author.id)
     },
     
+    // Bot's owner. Only the Bot's owner should have this as it gives access to potentially
+    // dangerous commands such as ?eval
     { level: 10,
       name: "Bot Owner", 
       check: (message) => message.client.config.ownerID === message.author.id
     }
   ]
-}
+};
+
+module.exports = config;
