36d03e3f7f72ddc796c17215696956568fe0e1ef
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-28 17:07:35 +1000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -33,8 +33,8 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
   aliases: [],
+  permLevel: "Bot Owner"
 };
 
 exports.help = {
