21fe5d3ff15c76947a0d5a5ec0c25ec313291387
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-29 13:05:13 +1000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,23 @@
-exports.run = (client, message, params) => {
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
+exports.run = (client, message, args, level) => {
+  
+   if (!args[0]) {
+    const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level);
+
+    const commandNames = myCommands.keyArray();
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.send("```" + `\nUse ${client.config.prefix}help [commandName] for usage information ~\n${client.commands.map(c => `${client.config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)}: ${c.help.description}`).join('\n')}` + "```");
+
+    let output = `[Use ${client.config.prefix}help <commandname> for details]\n`;
+    const sorted = myCommands.array().sort((p, c) => p.help.name > c.help.name ? 1 : -1 );
+    sorted.forEach( c => {
+      output += `${message.settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)}: ${c.help.description}\n`;
+    });
+    message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
   } else {
-    let command = params[0];
+    let command = args[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.send("```" + `\n**Name:** ${command.help.name}\n**Description:** ${command.help.description}\n**Usage:** ${command.help.usage}\n` + "```");
+      if (level < client.levelCache[command.conf.permLevel]) return;
+      message.channel.send(`**${command.help.name}**\n${command.help.description}\nusage: ${command.help.usage}\naliases: ${command.conf.aliases.join(", ")}`, {code:"asciidoc"});
     }
   }
 };
