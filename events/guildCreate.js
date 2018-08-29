d9f1850d965f52b6641047cd5e14d518b0b48659
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-29 11:21:26 +1000

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,38 +1,40 @@
 module.exports = (client, guild) => {
-let defaultChannel = "";
-guild.channels.forEach((channel) => {
-  if(channel.type == "text" && defaultChannel == "") {
-    if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-      defaultChannel = channel;
+  
+  let defaultChannel = "";
+  guild.channels.forEach((channel) => {
+    if(channel.type == "text" && defaultChannel == "") {
+      if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+        defaultChannel = channel;
+      }
     }
-  }
-})
-defaultChannel.send(`*Squeak!* Thanks for inviting me to your server! H-Here's some info about me, if you're interested. ◕ ◡ ◕`, {
-embed:{
-    title: 'Hamtaro Bot',
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    color: 0x2471a3, 
-    description: "Use ?help to see my commands! ^-^",
-    fields: [
-        {
-            name: '**My Master:**',
-            value: 'High-Fox'
-        },     
-        {
-            name: 'My Code:',
-            value: '[GitHub Source Code](https://github.com/High-Fox/Hamtoro-Bot/)'
-        },
-        {
-            name: 'My Home:',
-            value: '[INSERT SERVER LINK HERE]'
-        } 
-    ],
-    footer: {
-        text: 'Hamtaro Bot © High-Fox 2018'
+  })
+  defaultChannel.send(`*Squeak!* Thanks for inviting me to your server! H-Here's some info about me, if you're interested. ◕ ◡ ◕`, {
+  embed:{
+      author: {
+        name: client.user.username,
+      },
+      color: 0x2471a3, 
+      thumbnail: {
+        url: client.user.avatarURL,
+      },
+      description: "Use ?help to see my commands! ^-^",
+      fields: [
+          {
+              name: '**My Master:**',
+              value: 'HighFox'
+          },     
+          {
+              name: 'My Code:',
+              value: '[GitHub Source Code](https://github.com/High-Fox/Hamtoro-Bot/)'
+          },
+          {
+              name: 'My Home:',
+              value: '[INSERT SERVER LINK HERE]'
+          } 
+      ],
+      footer: {
+          text: 'Hamtaro Bot © High-Fox 2018'
+      }
     }
-   }
- })
+  })
 };
