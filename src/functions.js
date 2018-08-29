d32dc9a9ad1dcbc05f6dc0c607965ab1d75828ce
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-29 13:35:38 +1000

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -154,6 +154,43 @@ return bearLinks[randomBearLink];
     }
   };
   
+  client.loadCommand = (commandName) => {
+    try {
+      const props = require(`../commands/${commandName}`);
+      console.log(`Loading Command: ${props.help.name}`);
+      if (props.init) {
+        props.init(client);
+      }
+      client.commands.set(props.help.name, props);
+      props.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, props.help.name);
+      });
+      return false;
+    } catch (e) {
+      return `Unable to load command ${commandName}: ${e}`;
+    }
+  };
+
+  client.unloadCommand = async (commandName) => {
+    let command;
+    if (client.commands.has(commandName)) {
+      command = client.commands.get(commandName);
+    } else if (client.aliases.has(commandName)) {
+      command = client.commands.get(client.aliases.get(commandName));
+    }
+    if (!command) return `The command \`${commandName}\` doesn"t seem to exist, nor is it an alias.`;
+  
+    if (command.shutdown) {
+      await command.shutdown(client);
+    }
+    delete require.cache[require.resolve(`../commands/${commandName}.js`)];
+    return false;
+  };
+  
+  String.prototype.toProperCase = function() {
+    return this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }; 
+  
   client.wait = require("util").promisify(setTimeout);
   
   process.on("uncaughtException", (err) => {
