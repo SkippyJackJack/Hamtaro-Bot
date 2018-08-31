b39be1ac26d7f6f67223021c133d8d489d494d79
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-31 11:53:26 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -3,41 +3,38 @@ exports.run = async (client, message, args, level) => {
 const fs = require('fs');
 const Jimp = require('jimp');
     
-    message.channel.startTyping();
-    
-    let image = "https://imgflip.com/s/meme/Ancient-Aliens.jpg";
-    
     let text = args.join(" ");
     
     if (!text) {
         return message.reply("But you have to give me text ._.");
     };
     
-    Jimp.read(image).then(function (image) {
+    message.channel.startTyping();
+    
+    Jimp.read('https://imgflip.com/s/meme/Ancient-Aliens.jpg').then(function (image) {     
+        image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
         
-    image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
-        
-      Jimp.loadFont(Jimp.FONT_SANS_128_WHITE).then(function(font) {
-        image.print(font, 240, 578, text.toUpperCase(), Jimp.ALIGN_FONT_CENTER).getBuffer(Jimp.MIME_JPEG, nullFunction)
+        Jimp.loadFont(../src/fonts/impact.fnt).then(function(font) {
+          image.print(font, 240, 578, text.toUpperCase(), Jimp.ALIGN_FONT_CENTER).getBufferAsync(Jimp.MIME_JPEG);
 
         let outputfile = "./" + Math.random().toString(15).substr(2, 5) + "." + image.getExtension();
-        image.write(outputfile, function () {
-          message.channel.send("Nice memeing ~", {
-                            files: [{
-                                attachment: outputfile,
-                                name: "output.jpg"
-                            }]
-                        }).then(function () {
-            fs.unlink(outputfile, (err) => {
-                              if (err) throw err;
-                              console.log("Image Created");
+            image.write(outputfile, function () {
+                
+                message.channel.send("Nice memeing ~", { files: [{ attachment: outputfile, name: "output.jpg" }]})
+                   .then(function () {
+                fs.unlink(outputfile, (err) => {
+                    if (err) throw err;
+                    console.log("Image Created");
+                    message.channel.stopTyping();
             });
           });
         });
       });
     }).catch(function (err) {
       console.error(err);
-      message.reply(";-; I-I couldn't create the image, sorry.")
+      message.channel.send(";-; I-I couldn't create the image, sorry.")
+        
+      message.channel.stopTyping();
     })
     
     function nullFunction(err) {
