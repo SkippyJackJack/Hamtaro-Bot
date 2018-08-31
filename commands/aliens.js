66d1e0c624cba967da989fefed7192e700e27d46
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-31 13:19:14 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -14,7 +14,7 @@ const Jimp = require('jimp');
     Jimp.read('https://imgflip.com/s/meme/Ancient-Aliens.jpg').then(function (image) {     
         image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
         
-        Jimp.loadFont('../src/fonts/impact.fnt').then(function(font) {
+        Jimp.loadFont(../src/fonts/impact.fnt).then(function(font) {
           image.print(font, 240, 578, text.toUpperCase(), Jimp.ALIGN_FONT_CENTER).getBufferAsync(Jimp.MIME_JPEG);
 
         let outputfile = "./" + Math.random().toString(15).substr(2, 5) + "." + image.getExtension();
