759859c961eaacf7dc4270a3304a3c08683479f8
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-08-31 11:56:04 +1000

    Update aliens.js

--- commands/aliens.js
+++ commands/aliens.js
@@ -14,18 +14,18 @@ const Jimp = require('jimp');
     Jimp.read('https://imgflip.com/s/meme/Ancient-Aliens.jpg').then(function (image) {     
         image.resize(1024, 1024, Jimp.RESIZE_BEZIER);
         
-        Jimp.loadFont(../src/fonts/impact.fnt).then(function(font) {
+        Jimp.loadFont('../src/fonts/impact.fnt').then(function(font) {
           image.print(font, 240, 578, text.toUpperCase(), Jimp.ALIGN_FONT_CENTER).getBufferAsync(Jimp.MIME_JPEG);
 
         let outputfile = "./" + Math.random().toString(15).substr(2, 5) + "." + image.getExtension();
             image.write(outputfile, function () {
                 
-                message.channel.send("Nice memeing ~", { files: [{ attachment: outputfile, name: "output.jpg" }]})
-                   .then(function () {
-                fs.unlink(outputfile, (err) => {
-                    if (err) throw err;
-                    console.log("Image Created");
-                    message.channel.stopTyping();
+                message.channel.send("Nice memeing ~", { files: [{ attachment: outputfile, name: "output.jpg" }]}).then(function () {
+                    
+                    fs.unlink(outputfile, (err) => {
+                        if (err) throw err;
+                        console.log("Image Created");
+                        message.channel.stopTyping();
             });
           });
         });
@@ -36,10 +36,6 @@ const Jimp = require('jimp');
         
       message.channel.stopTyping();
     })
-    
-    function nullFunction(err) {
-         if (err) throw err;
-    }
     
     message.channel.stopTyping();
 };
