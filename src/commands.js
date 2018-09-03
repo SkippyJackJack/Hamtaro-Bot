2ac197b55f036ee804547ba20960d29c5c293877
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-03 10:07:25 +1000

    Create commands.js

---
+++ src/commands.js
@@ -0,0 +1,21 @@
+module.exports = [
+  
+  "about",
+  "aliens",
+  "changemymind",
+  "decide",
+  "diceroll",
+  "eval",
+  "growl",
+  "help",
+  "hibernation",
+  "hoot",
+  "invite",
+  "ping",
+  "serverinfo",
+  "setconf",
+  "squeak",
+  "suggestion",
+  "userinfo"
+
+]
