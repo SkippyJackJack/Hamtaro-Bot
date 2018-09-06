210bf17567db0026f839a0b40836ad7e6da91627
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-06 13:14:43 +1000

    Update randomImageFunctions.js

--- src/misc/randomImageFunctions.js
+++ src/misc/randomImageFunctions.js
@@ -5,27 +5,27 @@ client.getRandomImage = (animal) => {
 if (animal == "hamster") {
   
 const hamsterLinks = [
-  "https://media.giphy.com/media/3oeHLrjZGBgnPx5VII/giphy.gif",
-  "https://media.giphy.com/media/kNGHx5m4QPra0/giphy.gif",
-  "https://media.giphy.com/media/IThjAlJnD9WNO/giphy.gif",
-  "https://media.giphy.com/media/JglVCaB0axZ4Y/giphy.gif",
-  "https://media.giphy.com/media/uUs14eCA2SBgs/giphy.gif",
-  "https://media.giphy.com/media/AaqdLBKYfZl0A/giphy.gif",
-  "https://media.giphy.com/media/ia7kRlpGe3IFq/giphy.gif",
-  "https://media.giphy.com/media/149c3jrzGk0AtG/giphy.gif",
-  "https://media.giphy.com/media/7jdcmQ9zb9Tk4/giphy.gif",
-  "https://media.giphy.com/media/anu91JH5LYiVW/giphy.gif",
-  "https://media.giphy.com/media/aK4wh0UE3oddS/giphy.gif",
-  "https://media.giphy.com/media/XCsuZw0hWj8B2/giphy.gif",
-  "https://media.giphy.com/media/Gd3PdO3AtmwWk/giphy.gif",
-  "https://media.giphy.com/media/tOWyML1WPzKjm/giphy.gif",
-  "https://media.giphy.com/media/3oeHLrjZGBgnPx5VII/giphy.gif",
-  "https://media.giphy.com/media/OBpFd1Dat9Vzq/giphy.gif",
-  "https://media.giphy.com/media/xUn3CmI6rN5pEUlwNa/giphy.gif",
-  "https://media.giphy.com/media/26n6WYBZZRSewlgd2/giphy.gif",
-  "https://media.giphy.com/media/Osub6rry9Pu2k/giphy.gif",
-  "https://media.giphy.com/media/YCCDVrmnXj8vC/giphy.gif",
-  "https://media.giphy.com/media/vYtDDrgTXyrm0/giphy.gif"
+  "https://media.giphy.com/media/w2fjYsGvjDF3a/giphy.gif",
+  "https://media.giphy.com/media/7Z71Z76pCC8Za/giphy.gif",
+  "https://media.giphy.com/media/Yv0Bwwlcnplu/giphy.gif",
+  "https://media.giphy.com/media/pPrUNsOY8CDS0/giphy.gif",
+  "https://media.giphy.com/media/YUx07ixq8GCSA/giphy.gif",
+  "https://media.giphy.com/media/wZ2SI2e3t1gR2/giphy.gif",
+  "https://media.giphy.com/media/p3wMMqkPBFqsEGI32T/giphy.gif",
+  "https://media.giphy.com/media/d1E2IByItLUuONMc/giphy.gif",
+  "https://media.giphy.com/media/E0KmHELTpq9pK/giphy.gif",
+  "https://media.giphy.com/media/mTRpdks4k5sgo/giphy.gif",
+  "https://media.giphy.com/media/jU2ZYjA8ngdKU/giphy.gif",
+  "https://media.giphy.com/media/GXnaqmGcg1CTu/giphy.gif",
+  "https://media.giphy.com/media/xT0BKDIwzgPHWRFDm8/giphy.gif",
+  "https://78.media.tumblr.com/42120f9738c3c8bb2e20754c3606cca6/tumblr_pb1k13a0JZ1tf2doho1_500.gif",
+  "https://media.giphy.com/media/JdCz7YXOZAURq/giphy.gif",
+  "https://media.giphy.com/media/ETTzs1c6mS36g/giphy.gif",
+  "https://media.giphy.com/media/ncU3bkZ5ghDlS/giphy.gif",
+  "https://media.giphy.com/media/TkUZUZ9E6KhJC/giphy.gif",
+  "https://media.giphy.com/media/L2vRqCV1lPTq0/giphy.gif",
+  "https://media.giphy.com/media/uTIZmj69qCjCM/giphy.gif",
+  "https://media.giphy.com/media/M4trH1uta8OPu/giphy.gif"
 ];
 
 var randomHamsterLink = Math.floor(Math.random() * hamsterLinks.length);
