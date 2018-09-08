47d66754a8b457dbfd8f79bea1ff7c6722a9b280
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-08 11:35:22 +1000

    Update ownerslover.js

--- commands/ownerslover.js
+++ commands/ownerslover.js
@@ -1,6 +1,6 @@
 exports.run = (client, message) => {
 
-    messsage.channel.send( { embed: { color: 0xf45042, description: `My Owner is currently in love with: <@437514679636459520>`, thumbnail: { url: "https://images-ext-2.discordapp.net/external/68uAi5MBRxWbZ6zOaFK2lQPe1qMCFfTpsElgyCcQm0c/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/437514679636459520/2e9013977612d4ae3d1049f914cf1ee5.png" }, } } );
+    message.channel.send( { embed: { color: 0xf45042, description: `My Owner is currently in love with: <@437514679636459520>`, thumbnail: { url: "https://images-ext-2.discordapp.net/external/68uAi5MBRxWbZ6zOaFK2lQPe1qMCFfTpsElgyCcQm0c/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/437514679636459520/2e9013977612d4ae3d1049f914cf1ee5.png" }, } } );
 };
 
 exports.conf = {
