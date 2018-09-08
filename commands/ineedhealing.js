384c71c7aec608446529066ae21600fb1216e9cb
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-08 11:38:54 +1000

    Create ineedhealing.js

---
+++ commands/ineedhealing.js
@@ -0,0 +1,15 @@
+exports.run = (client, message, args) => {
+
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "ineedhealing",
+  description: "Someone needs healing...",
+  usage: "ineedhealing"
+};
