ab3f416aefbff9538710074a1a47a5adfc93e1f1
High-Fox <41532580+High-Fox@users.noreply.github.com>
2018-09-08 11:29:46 +1000

    Update ownerslover.js

--- commands/ownerslover.js
+++ commands/ownerslover.js
@@ -2,7 +2,7 @@ exports.run = (client, message) => {
   
   client.fetchUser(437514679636459520).then(bb => {
     messsage.channel.send( { embed: { color: 0xf45042, description: `My Owner is currently in love with: ${bb}`, thumbnail: { url: bb.avatarURL }, } } );
-  }
+  })
 };
 
 exports.conf = {
